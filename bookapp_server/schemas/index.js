9917590cf82149913d17f608598aa0140a7b7d66
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-01-13 20:14:06 -0600

    started new project

---
+++ bookapp_server/schemas/index.js
@@ -0,0 +1,25 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+    genre: { type: graphql.GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //code
+      },
+    },
+  },
+});
