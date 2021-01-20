8dd0b61c9d05169680e0cdabf40ce02f254d7ea0
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-01-19 20:48:17 -0600

    set up client

---
+++ bookapp_client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="App">
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
