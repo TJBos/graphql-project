61a2193b2319ce5277bed2dd427e5ebd9747345a
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-01-19 21:03:41 -0600

    added apollo provider

--- bookapp_client/src/App.js
+++ bookapp_client/src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- bookapp_client/src/components/BookList.js
+++ bookapp_client/src/components/BookList.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-const BookList = () => {
-  return <div></div>;
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const BookList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 };
+
+export default graphql(getBooksQuery)(BookList);
